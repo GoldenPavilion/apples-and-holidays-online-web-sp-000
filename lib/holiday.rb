0d13cbf13bae69a515fe57223a3698592eca013d
Robert Stine <rcorystine@gmail.com>
2020-12-23 23:16:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,9 +58,10 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   
-  holiday_hash.each do |season, holidays|
+  holiday_hash.each do |season, holiday|
     puts "#{season}:"
-    holidays.each do |holiday, items|
+    holiday.each do |holiday, items|
+      holiday.to_s.capitalize
       
     end
   end
