6b3dd979393af824db70cf527161f8e127517529
Robert Stine <rcorystine@gmail.com>
2020-12-23 23:19:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,6 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season}:"
     holiday.each do |holiday, items|
-      binding.pry
       puts "#{holiday.to_s.capitize}"
     end
   end
